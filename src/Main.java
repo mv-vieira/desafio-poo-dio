b6b11fd2958144a35399a89864a33a4f1301e3fd
Matheus Vicente <mv_vieira96@hotmail.com>
2023-04-18 20:35:20 -0300

    Trabalhando com abstração e encapsulamento.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso = new Curso("Java", "POO", 100);
+        System.out.println(curso);
+
+        Curso curso2 = new Curso("Angular", "Curso em Angular 15", 50);
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria("Mentoria Java", "Descrição Mentoria Java", LocalDate.now());
+        System.out.println(mentoria);
+
+        Mentoria mentoria2 = new Mentoria("Mentoria Angular", "Descrição Mentoria Angular", LocalDate.now());
+        System.out.println(mentoria2);
+
     }
 }
