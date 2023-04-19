8299759b0e85af0c24e1a26080e17ec4bf7cee3f
Matheus Vicente <mv_vieira96@hotmail.com>
2023-04-18 21:09:46 -0300

    Trabalhando com herança e polimorfismo (1).

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
@@ -7,16 +8,16 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
 
-        Curso curso = new Curso("Java", "POO", 100);
+        Conteudo curso = new Curso("Java", "POO", 100);
         System.out.println(curso);
 
-        Curso curso2 = new Curso("Angular", "Curso em Angular 15", 50);
+        Conteudo curso2 = new Curso("Angular", "Curso em Angular 15", 50);
         System.out.println(curso2);
 
-        Mentoria mentoria = new Mentoria("Mentoria Java", "Descrição Mentoria Java", LocalDate.now());
+        Conteudo mentoria = new Mentoria("Mentoria Java", "Descrição Mentoria Java", LocalDate.now());
         System.out.println(mentoria);
 
-        Mentoria mentoria2 = new Mentoria("Mentoria Angular", "Descrição Mentoria Angular", LocalDate.now());
+        Conteudo mentoria2 = new Mentoria("Mentoria Angular", "Descrição Mentoria Angular", LocalDate.now());
         System.out.println(mentoria2);
 
     }
