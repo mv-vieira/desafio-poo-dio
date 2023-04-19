3534ee6ae25a935156001e271ea46743310f4e57
Matheus Vicente <mv_vieira96@hotmail.com>
2023-04-18 22:15:31 -0300

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -9,16 +7,53 @@ public class Main {
     public static void main(String[] args) {
 
         Conteudo curso = new Curso("Java", "POO", 100);
-        System.out.println(curso);
+//        System.out.println(curso);
 
         Conteudo curso2 = new Curso("Angular", "Curso em Angular 15", 50);
-        System.out.println(curso2);
+//        System.out.println(curso2);
 
         Conteudo mentoria = new Mentoria("Mentoria Java", "Descrição Mentoria Java", LocalDate.now());
-        System.out.println(mentoria);
+//        System.out.println(mentoria);
 
         Conteudo mentoria2 = new Mentoria("Mentoria Angular", "Descrição Mentoria Angular", LocalDate.now());
-        System.out.println(mentoria2);
+//        System.out.println(mentoria2);
+
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java / Angular Developer");
+        bootcamp.setDescricao("Um Bootcamp desenvolvido pelo Banco Pan");
+        bootcamp.getConteudos().add(curso);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+        bootcamp.getConteudos().add(mentoria2);
+
+        Dev devMatheus = new Dev();
+        devMatheus.setNome("Matheus");
+        devMatheus.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Matheus: " + devMatheus.getConteudosInscritos());
+        System.out.println("-");
+        devMatheus.progredir();
+        devMatheus.progredir();
+        devMatheus.progredir();
+        devMatheus.progredir();
+        System.out.println("Conteúdos Inscritos após progressão Matheus: " + devMatheus.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Matheus: " + devMatheus.getConteudosConcluidos());
+        System.out.println("Xp atual: " + devMatheus.calcularTotalXp());
+
+
+        System.out.println("--------------");
+
+
+        Dev devMikaela = new Dev();
+        devMikaela.setNome("Mikaela");
+        devMikaela.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Mikaela: " + devMikaela.getConteudosInscritos());
+        System.out.println("-");
+        devMikaela.progredir();
+        System.out.println("Conteúdos Inscritos após progressão Mikaela: " + devMikaela.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Mikaela: " + devMikaela.getConteudosConcluidos());
+        System.out.println("Xp atual: " + devMikaela.calcularTotalXp());
+
 
     }
 }
